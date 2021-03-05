6e47de864db9c1ee0bdecd481a285035da76445e
mamewharu <mame0040007@gmail.com>
2021-03-05 18:08:44 +0900

    アソシェーションとプルダウン実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,6 @@ class User < ApplicationRecord
           validates :occupation
          end
 
-   has_many :rooms, through: :room_users
    has_many :room_users
+   has_many :rooms, through: :room_users
 end
