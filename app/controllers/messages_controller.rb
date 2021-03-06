df03f6584d582a16c187b9cc1871d927c7eafa11
mamewharu <mame0040007@gmail.com>
2021-03-06 10:51:09 +0900

    ルーディングのネスト

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,13 @@
 class MessagesController < ApplicationController
   def index
+    @messages = Message.all
   end
+
+  def create
+    @message = Message.new
+  #   if @message.save
+  #     redirect_to :index
+  #   else
+  #     render :createend
+  # end
 end
