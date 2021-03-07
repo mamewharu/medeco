9dc50363ecf52172c2fb682a1cf356d4f469e535
mamewharu <mame0040007@gmail.com>
2021-03-07 22:15:06 +0900

    修正

--- config/application.rb
+++ config/application.rb
@@ -10,12 +10,6 @@ module Medeco
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-    config.generators do |g|
-      g.stylesheets false
-      g.javascripts false
-      g.helper false
-      g.test_framework false
-    end
     config.i18n.default_locale = :ja
     config.time_zone = 'Tokyo'
 
